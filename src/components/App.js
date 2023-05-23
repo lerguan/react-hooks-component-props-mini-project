bf2d216780bf8047fc214d242e1d84236d0669b6
Leroy G <liguan698@gmail.com>
2023-05-23 00:12:10 -0700

    remove import logo

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
 import ArticleList from "./ArticleList";
-import logo from "../assets/logo.svg"
+
 
 function App() {
   return (
